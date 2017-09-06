efa65b30d8a1e48081df1b075151581b0e3b7321
evanzmx <cruz.arellano.oscar@gmail.com>
2017-09-05 20:11:46 -0500

    PDF
    Se agrega la versión PDF y se corrigen algunas cosas del texto

--- main.js
+++ main.js
@@ -2,16 +2,43 @@ $(function(){
     if(localStorage.languaje === undefined){
         //Setting default languaje as Spanish
         localStorage.languaje = 'SPA';
+        $("#btn_download").attr({
+            href: 'Oscar_Cruz_CV.pdf',
+            title: 'Oscar Cruz CV'
+        });
+        $("#btn_download").text('Descarga una versión PDF.');
+    }else if (localStorage.languaje == 'SPA') {
+        $("#btn_download").attr({
+            href: 'Oscar_Cruz_CV.pdf',
+            title: 'Oscar Cruz CV'
+        });
+        $("#btn_download").text('Descarga una versión PDF.');
+    }else if (localStorage.languaje == 'ENG') {
+        $("#btn_download").attr({
+            href: 'Oscar_Cruz_Resume.pdf',
+            title: 'Oscar Cruz Resume'
+        });
+        $("#btn_download").text('Download a PDF version.');
     }
     getContent();
     
     $("#btnlang").click(function (e) {
         if (localStorage.languaje == 'SPA') {
             localStorage.languaje = 'ENG';
-            $().text('ES');
+            $().text('Versión en Español');
+            $("#btn_download").attr({
+                href: 'Oscar_Cruz_Resume.pdf',
+                title: 'Oscar Cruz Resume'
+            });
+            $("#btn_download").text('Download a PDF version.');
         } else {
             localStorage.languaje='SPA';
-            $().text("EN");
+            $().text("English version");
+            $("#btn_download").attr({
+                href: 'Oscar_Cruz_CV.pdf',
+                title: 'Oscar Cruz CV'
+            });
+            $("#btn_download").text('Descarga una versión PDF.');
         }
         getContent();
     });
